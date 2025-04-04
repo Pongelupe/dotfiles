5c7b1cb1fac3b46fb0cc1b47105e907c68ebdb88
pongelupe <pongelupe1998@gmail.com>
2025-04-04 11:51:01 -0300

    adds script to decrypt all secrets

---
+++ scripts/unlock_secrets.sh
@@ -0,0 +1,26 @@
+# !/bin/bash
+
+echo "Executing a lockpick test..."
+cd ~/workspace/dotfiles/secrets
+cat skeleton_key | age -d > .key.tmp  2> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Your pick has broken, Dovahkiin!" >&2
+    exit 1  # exit
+fi
+
+echo "Chest opened!"
+
+echo "Using skeleton key to unlock all chests"
+
+for file in $(find . -type f -name "*.age"); do
+    echo "Processing: $file"
+    name="${file%.*}"
+    cat $file | age -d -i .key.tmp > $name
+    echo "Created: $name"
+done
+
+cat security.age |  age -d -i .key.tmp
+
+# checkout operations
+rm .key.tmp

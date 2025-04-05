46abb486f2fc0d5873b847eeb56850b7655f1672
pongelupe <pongelupe1998@gmail.com>
2025-04-04 22:41:45 -0300

    updates dotfiles and binding

--- scripts/bind.sh
+++ scripts/bind.sh
@@ -1,7 +1,11 @@
 # !/bin/bash
 
+ln -s "$(pwd)/../.aliases" ~/.aliases
+ln -s "$(pwd)/../.config" ~/.config
+ln -s "$(pwd)/../.exports" ~/.exports
+ln -s "$(pwd)/../.gitconfig" ~/.gitconfig
+ln -s "$(pwd)/../.path" ~/.path
+ln -s "$(pwd)/../.ssh" ~/.ssh
 ln -s "$(pwd)/../.vim" ~/.vim
 ln -s "$(pwd)/../.vimrc" ~/.vimrc
-ln -s "$(pwd)/../.aliases" ~/.aliases
-ln -s "$(pwd)/../.exports" ~/.exports
-ln -s "$(pwd)/../.ssh" ~/.ssh
+ln -s "$(pwd)/../.zshrc" ~/.zshrc

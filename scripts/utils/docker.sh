0fce7d84cc8551d2b531d13d0a71a502a1692d56
pongelupe <pongelupe1998@gmail.com>
2025-11-17 20:12:37 -0300

    update docker.sh

--- scripts/utils/docker.sh
+++ scripts/utils/docker.sh
@@ -5,3 +5,9 @@ build_dockerfile_with_name() {
     echo "Build Dockerfile called $name"
     docker build -t "$name" .
 }
+
+stop_remove_container_with_name() {
+    name=$1
+    docker stop "$name"
+    docker rm "$name"
+}

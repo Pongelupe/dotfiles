7101a7a7f5c089618bd461c33a3b6b2e998acc6b
pongelupe <pongelupe1998@gmail.com>
2026-01-28 14:15:21 -0300

    adiciona .tmux.conf

--- scripts/bind.sh
+++ scripts/bind.sh
@@ -9,6 +9,7 @@ for file in "$DOT_FILES"/.config/*; do
 	ln -sf "$file" "$HOME/.config/$(basename "$file")" 
 done
 
+ln -sf "$DOT_FILES/.tmux.conf" "$HOME/.tmux.conf"
 ln -sf "$DOT_FILES/.exports" "$HOME/.exports"
 ln -sf "$DOT_FILES/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DOT_FILES/.path" "$HOME/.path"
